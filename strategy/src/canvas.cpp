99d9e82767ba22d22612523744aaf057dfcb40c8
Hopson97 <mhopson@hotmail.co.uk>
2019-08-06 21:02:12 +0100

    Implement canvas class

--- strategy/src/canvas.cpp
+++ strategy/src/canvas.cpp
@@ -1,1 +1,18 @@
 #include "canvas.h"
+
+#include "constants.h"
+
+Canvas::Canvas()
+:   m_renderCanvas({WIDTH, HEIGHT}) {
+    m_canvas.create(WIDTH, HEIGHT);
+    m_canvasTexture.loadFromImage(m_canvas);
+    m_renderCanvas.setTexture(&m_canvasTexture);
+}
+
+void Canvas::update() {
+    m_canvasTexture.loadFromImage(m_canvas);
+}
+
+void Canvas::render(sf::RenderWindow& window) {
+    window.draw(m_renderCanvas);
+}
--- strategy/src/canvas.h
+++ strategy/src/canvas.h
@@ -5,12 +5,33 @@
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 
+/**
+ * @brief The class that holds information about the image
+ * the user is drawing onto
+ * 
+ */
 class Canvas {
     public:
+        /**
+         * @brief Construct a new Canvas object
+         * Initilizes data required for canvas to operate
+         */
         Canvas();
 
+        /**
+         * @brief Update the canvas image to display what has been drawn since last update
+         * 
+         */
         void update();
+
+        /**
+         * @brief Render the canvas image onto the window
+         * 
+         * @param window The window 
+         */
         void render(sf::RenderWindow& window);
+
+        
 
     private:
         sf::Image m_canvas;
