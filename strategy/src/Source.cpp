744aa056366a9047bb9a789e9fff50230c991422
Hopson97 <mhopson@hotmail.co.uk>
2019-08-16 20:55:21 +0100

    Add strategy classes

---
+++ strategy/src/Source.cpp
@@ -0,0 +1,16 @@
+#include "tool_type_strategy.h"
+
+#include "../canvas.h"
+
+void PaintBrushStrategy::handleMouseDown(sf::Event e, Canvas& canvas) {
+
+}
+
+void PaintBrushStrategy::handleMouseUp(sf::Event e, Canvas& canvas) {
+
+}
+
+void PaintBrushStrategy::handleMouseMove(sf::Event e, Canvas& canvas) {
+
+}
+
---
+++ strategy/src/tool_strategy/pencil_tool_strategy.cpp
@@ -0,0 +1,16 @@
+#include "tool_type_strategy.h"
+
+#include "../canvas.h"
+
+void PencilToolStrategy::handleMouseDown(sf::Event e, Canvas& canvas) {
+
+}
+
+void PencilToolStrategy::handleMouseUp(sf::Event e, Canvas& canvas) {
+
+}
+
+void PencilToolStrategy::handleMouseMove(sf::Event e, Canvas& canvas) {
+
+}
+
