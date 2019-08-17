796e21a38c63c61d4724bdabff6d83cd3660e035
Hopson97 <mhopson@hotmail.co.uk>
2019-08-17 08:44:44 +0100

    Create files for rest of tools

---
+++ strategy/src/tool_strategy/line_tool_strategy.cpp
@@ -0,0 +1,19 @@
+#include "tool_type_strategy.h"
+
+#include "../canvas.h"
+
+void LineToolStrategy::handleMouseDown(sf::Event e, Canvas& canvas) {
+
+}
+
+void LineToolStrategy::handleMouseUp(sf::Event e, Canvas& canvas) {
+
+}
+
+void LineToolStrategy::handleMouseMove(sf::Event e, Canvas& canvas) {
+
+}
+
+void LineToolStrategy::renderPreview(sf::RenderWindow& window) {
+    
+}
---
+++ strategy/src/tool_strategy/square_tool_strategy.cpp
@@ -0,0 +1,19 @@
+#include "tool_type_strategy.h"
+
+#include "../canvas.h"
+
+void SquareToolStrategy::handleMouseDown(sf::Event e, Canvas& canvas) {
+
+}
+
+void SquareToolStrategy::handleMouseUp(sf::Event e, Canvas& canvas) {
+
+}
+
+void SquareToolStrategy::handleMouseMove(sf::Event e, Canvas& canvas) {
+
+}
+
+void SquareToolStrategy::renderPreview(sf::RenderWindow& window) {
+    
+}
