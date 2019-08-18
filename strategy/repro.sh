9fc1bcc8b7f272046029f48fb7bdb7092108a056
Miguel Mendes <miguelmendeseiriz@gmail.com>
2019-08-18 23:37:41 +0100

    removed repro.

--- strategy/repro.sh
+++
@@ -1,9 +0,0 @@
-docker run -it ubuntu
-cd
-apt update
-apt install git cmake build-essential libsfml-dev --assume-yes # 600 MB download
-git clone https://github.com/Hopson97/design-patterns-explained.git
-cd design-patterns-explained/strategy/
-chmod 764 scripts/*.sh # we should probably make a PR to fix these files permissions
-./scripts/build.sh
-
