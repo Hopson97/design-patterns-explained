9d9aa24f3c0c1ba29b1ce97bfb7ccdf198e43d86
Hopson97 <mhopson@hotmail.co.uk>
2019-10-05 16:12:04 +0100

    Make the main server use the controllers

---
+++ mvc/app/controllers/comment.controller.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const express = require('express');
+
+const router = express.Router();
+
+
+module.exports = {
+    router: router
+}
--- mvc/app/index.js
+++ mvc/app/index.js
@@ -2,12 +2,20 @@ const express = require("express")
 const ejslayout = require("express-ejs-layouts");
 const path = require('path');
 
+const comments = require('./controllers/comment.controller');
+const posts = require('./controllers/post.controller');
+const users = require('./controllers/user.controller');
+
 //Set up the application
 const app = express();
 app.use(express.static(`./public/`));
 app.use(ejslayout);
 app.set(`view engine`, `ejs`);
 app.set(`views`, path.join(__dirname, "/../public/views/"));
+
+app.use("/comments/", comments.router);
+app.use("/posts/", posts.router);
+app.use("/users/", users.router);
 
 //Routes
 app.get(`/`, (request, response) => {
